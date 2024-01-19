dc170f03d9fa7f0117131c89aff55be0064ac6ba
Ketbome <pablo.moraga@mail.udp.cl>
2024-01-18 22:27:27 -0300

    Initial commit

---
+++ migrations/2024_01_18_214257_create_historicos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoricosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historicos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_usuario');
+            $table->unsignedBigInteger('id_vehiculo');
+            $table->timestamp('fecha_cambio')->useCurrent();
+            $table->foreign('id_vehiculo')->references('id')->on('vehiculos')->onDelete('cascade');
+            $table->foreign('id_usuario')->references('id')->on('usuarios')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historicos');
+    }
+}

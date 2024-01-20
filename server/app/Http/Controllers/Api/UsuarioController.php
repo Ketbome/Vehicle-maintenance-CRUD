4e2e0f17995258b946dcb2346d33ecefbc38cbbc
Ketbome <pablo.moraga@mail.udp.cl>
2024-01-20 01:41:18 -0300

    feat: :memo: Agregado de metodos para el modelo de usuario

--- server/app/Http/Controllers/Api/UsuarioController.php
+++ server/app/Http/Controllers/Api/UsuarioController.php
@@ -4,61 +4,43 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Usuario;
 
 class UsuarioController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        return Usuario::all();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+    $validatedData = $request->validate([
+        'nombre' => 'required|max:255',
+        'apellido' => 'required|max:255',
+        'email' => 'required|email|unique:usuarios',
+    ]);
+
+    $recurso = Usuario::create($validatedData);
+    return response()->json($recurso, 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $usuario = Usuario::findOrFail($id);
+        return response()->json($usuario);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $usuario = Usuario::findOrFail($id);
+        $usuario->update($request->all());
+        return response()->json($usuario);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        Usuario::destroy($id);
+        return response()->json(null, 204);
     }
 }

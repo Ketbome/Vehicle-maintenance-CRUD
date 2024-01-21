5ec3b4e69b29928acf604993acc57ba258493773
Ketbome <pablo.moraga@mail.udp.cl>
2024-01-21 16:25:13 -0300

    fix: :bug: Cambios en creacion de vehiculo guardado en historial y en update

--- server/app/Http/Controllers/Api/VehiculoController.php
+++ server/app/Http/Controllers/Api/VehiculoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Vehiculo;
+use App\Models\Historico;
 
 class VehiculoController extends Controller
 {
@@ -26,8 +27,18 @@ class VehiculoController extends Controller
             'precio' => 'required|numeric'
         ]);
 
-        $recurso = Vehiculo::create($validatedData);
-        return response()->json($recurso, 201);
+        
+        // Crear el nuevo vehículo
+        $vehiculo = Vehiculo::create($validatedData);
+
+        // Crear un registro en el historial para el nuevo vehículo
+        Historico::create([
+            'id_vehiculo' => $vehiculo->id,
+            'id_usuario' => $vehiculo->id_usuario,
+            'fecha_cambio' => now(), // La fecha de creación del vehículo
+        ]);
+
+        return response()->json($vehiculo, 201);
     }
 
     public function show($id)
@@ -39,12 +50,23 @@ class VehiculoController extends Controller
     public function update(Request $request, $id)
     {
         $validatedData = $request->validate([
-            'modelo' => 'sometimes:required|max:255',
-            'id_usuario' => 'sometimes|exists:required|integer',
-            'precio' => 'sometimes:required|numeric'
+            'modelo' => 'sometimes|required|max:255',
+            'id_usuario' => 'sometimes|exists:usuarios,id|integer',
+            'precio' => 'sometimes|required|numeric'
         ]);
 
         $vehiculo = Vehiculo::findOrFail($id);
+
+        // Comprueba si el id_usuario está cambiando
+        if ($request->has('id_usuario') && $vehiculo->id_usuario != $request->id_usuario) {
+            // Crear un nuevo registro en el historial
+            Historico::create([
+                'id_usuario' => $request->id_usuario,
+                'id_vehiculo' => $id,
+                'fecha_cambio' => now(),
+            ]);
+        }
+
         $vehiculo->update($validatedData);
         return response()->json($vehiculo);
     }
